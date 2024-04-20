6738d4d9ecea6ec81635ecc5c1546a5ce1cf5993
ze12000 <167222274+ze12000@users.noreply.github.com>
2024-04-20 18:04:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+from pyrogram import  filters,enums
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import InlineKeyboardMarkup as mk, InlineKeyboardButton as btn
+from pyrogram.types import ChatPermissions
+
+from asSQL import Client as cl
+
+
+data = cl("protect")
+db = data['data']
+db.create_table()
+db.set("botname",['عهد' , 'عهود' , 'بوت' ,'عاهد' , 'عهو'])
+db.set("bad_words",['كس','عير','طيز','زب','كسمك','كسختك','طيزك','مص'])
+
+plugins = dict(root="plugins")
+
+Client("x",
+api_id=3895828,
+api_hash="5229902a14b2512c35688aa152bd9f29",
+bot_token="6036463472:AAEpHcyWl42g-ga6HwSzWdUq5NVpu4rdBL4", plugins=plugins).run()
